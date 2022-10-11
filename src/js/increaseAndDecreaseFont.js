942c6d7cc3331965e4b12c3ef9d318ed6bd0605f
luciana-pereira <rm92014@fiap.com.br>
2022-10-10 21:02:10 -0300

    Função de acessibilidade alterado.

--- src/js/increaseAndDecreaseFont.js
+++ src/js/increaseAndDecreaseFont.js
@@ -1,21 +1,22 @@
-window.onload = function() {
-    var elementBody = document.querySelector('body');
-    var elementBtnIncreaseFont = document.getElementById('increase-font');
-    var elementBtnDecreaseFont = document.getElementById('decrease-font');
-    // Padrão de tamanho, equivale a 100% do valor definido no Body
-    var fontSize = 100;
-    // Valor de incremento ou decremento, equivale a 10% do valor do Body
-    var increaseDecrease = 10;
 
-    // Evento de click para aumentar a fonte
-    elementBtnIncreaseFont.addEventListener('click', function(event) {
-        fontSize = fontSize + increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
-    });
+const  aumentarFonte = () => {
+  let elementHtml = document.querySelector("html");
+  elementHtml.style.fontSize = "20px";
+}
 
-    // Evento de click para diminuir a fonte
-    elementBtnDecreaseFont.addEventListener('click', function(event) {
-        fontSize = fontSize - increaseDecrease;
-        elementBody.style.fontSize = fontSize + '%';
-    });
+const diminuirFonte = () => {
+  let elementHtml = document.querySelector("html");
+  elementHtml.style.fontSize = "15px";
+}
+
+const dislexia = () => {
+  let elementBody = document.querySelector('body');
+  elementBody.style.fontFamily  = 'opendyslexicaltaregular';
+  elementBody.style.fontSize    = '15px';
+}
+
+const inter = () => {
+  let elementBody = document.querySelector('body');
+  elementBody.style.fontFamily  = 'Inter, Arial, Tahoma, sans-serif';
+  elementBody.style.fontSize    = '15px';
 }
